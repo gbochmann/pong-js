dbf6929132d8751b415444bbd9ea11c5196b913e
Gerome Bochmann <gerome.bochmann@gmail.com>
2016-08-16 19:46:28 +0200

    Moved collision stuff from common.js to own file

--- README.md
+++ README.md
@@ -4,8 +4,8 @@ A clone of the good old fashioned pong game written in JS
 
 # Todo
 
-* fix key delay issue
 * add proper collision detection for paddles i.e. the ball should only collide within the y-range of the paddle
+* fix key delay issue
 * refactor collision management i.e.
 	* move all collision stuff from common.js to their own file
-	* refactor *findCollision* if possible
+	* ~~refactor *findCollision* if possible~
--- js/common.js
+++ js/common.js
@@ -2,26 +2,6 @@ var hasMovement = { // eslint-disable-line no-unused-vars
 	move: move
 };
 
-var hasCollision = { // eslint-disable-line no-unused-vars
-	findCollision: function findCollsion(collidable){
-		var xDistance = norm(transpose(collidable.x + collidable.xBorder, this.x));
-		var yDistance = norm(transpose(collidable.y + collidable.yBorder, this.y));
-		var xCollison = this.xBorder > xDistance;
-		var yCollision = this.yBorder > yDistance;
-		this.isColliding = xCollison || yCollision;
-		if (this.isColliding) this.collisionReaction();
-	},
-	isColliding: false
-};
-
-var hasCollisionBounce = { // eslint-disable-line no-unused-vars
-	collisionReaction: function collisionBounce() {
-		console.log(this);
-		if (this.dx) this.dx = -this.dx;
-		if (this.dy) this.dy = -this.dy;
-	}
-};
-
 function norm(num) {
 	return num < 0 ? -num : num;
 }
@@ -43,17 +23,6 @@ function child (parent, child) { // eslint-disable-line no-unused-vars
     Proxy.prototype = parent.prototype;
     child.prototype = new Proxy();
     return child;
-}
-
-function collider (collidables) { // eslint-disable-line no-unused-vars
-	var listCopy = collidables.slice();
-	var target = listCopy.pop();
-	_.inject(listCopy, checkCollisions, target);
-}
-
-function checkCollisions(target, collidable) {
-	target.findCollision(collidable);
-	return target;
 }
 
 function registerKeyEvents () {
