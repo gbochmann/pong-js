6a284b434a49dd5dce79885bf5e7e32f36789a76
Gerome Bochmann <gerome.bochmann@gmail.com>
2016-08-22 20:07:11 +0200

    Added Gruntfile

---
+++ Gruntfile.js
@@ -0,0 +1,17 @@
+/* global require grunt */
+require("load-grunt-tasks")(grunt); // npm install --save-dev load-grunt-tasks
+
+grunt.initConfig({
+  "babel": {
+    options: {
+      sourceMap: true
+    },
+    dist: {
+      files: {
+        "dist/app.js": "src/app.js"
+      }
+    }
+  }
+});
+
+grunt.registerTask("default", ["babel"]);
