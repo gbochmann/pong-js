9d12e51e4678ea19e2ca9fa8ab2bc3373e28536f
Gerome Bochmann <gerome.bochmann@gmail.com>
2016-08-22 22:49:05 +0200

    Moved var statements to top of file

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,15 +1,16 @@
 /* global require module */
+var sources = ['src/*.es6'];
+var files = [{
+    expand: true,
+    cwd: 'src',
+    ext: '-es6.js',
+    src: '*.es6',
+    dest: 'dist/'
+}];
+
 module.exports = function (grunt) {
     require("load-grunt-tasks")(grunt, { pattern: ['grunt-*', '@*/grunt-*'] }); // npm install --save-dev load-grunt-tasks
 
-    var sources = ['src/*.es6'];
-    var files = [{
-        expand: true,
-        cwd: 'src',
-        ext: '-es6.js',
-        src: '*.es6',
-        dest: 'dist/'
-    }];
 
     grunt.initConfig({
         browserify: {
