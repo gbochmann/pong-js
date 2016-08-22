e1f1443c32cd3423a48e95e6392d4dea90532471
Gerome Bochmann <gerome.bochmann@gmail.com>
2016-08-22 22:07:59 +0200

    Added browserify configuration

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,25 +2,38 @@
 module.exports = function (grunt) {
     require("load-grunt-tasks")(grunt, { pattern: ['grunt-*', '@*/grunt-*'] }); // npm install --save-dev load-grunt-tasks
 
+    var sources = ['src/*.es6'];
+    var files = [{
+        expand: true,
+        cwd: 'src',
+        ext: '-es6.js',
+        src: '*.es6',
+        dest: 'dist/'
+    }];
+
     grunt.initConfig({
+        browserify: {
+            dist: {
+                options: {
+                    transform: [
+                        ["babelify"]
+                    ]
+                },
+                files: files
+            }
+        },
         babel: {
             options: {
                 sourceMap: true
             },
             dist: {
-                files: [{
-                    expand: true,
-                    cwd: 'src',
-                    ext: '-es6.js',
-                    src: '*.es6',
-                    dest: 'dist/'
-                }]
+                files: files
             }
         },
         watch: {
             scripts: {
-                files: ['src/*.es6'],
-                tasks: ['babel'],
+                files: sources,
+                tasks: ['browserify'],
                 options: {}
             }
         }
